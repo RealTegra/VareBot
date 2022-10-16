f40f5e032dd94adcaa8bf11d98d5b4307cad3a9d
Amnesia <75692156+AmnesiaReal@users.noreply.github.com>
2022-10-16 19:32:03 +0200

    Fixando
    Stiamo sistemando

--- plugins/autoadmin.js
+++
@@ -1,8 +0,0 @@
-import fs from 'fs'
-function handler(m, { conn }) {
-let text = `ora sei admin`.trim()   
-conn.reply(m.chat, text, m, {
-})
-}
-handler.command = /^(autoadmin)/i
-export default handler
--- plugins/random-meme.js
+++
@@ -1,9 +0,0 @@
-import fetch from 'node-fetch'
-let handler = async (m, { conn, command }) => {
-let url = packgirl[Math.floor(Math.random() * packgirl.length)]
-conn.sendButton(m.chat, `frocio`, author, url, ['tu sei un meme'], m)
-}
-handler.help = ['meme']
-handler.tags = ['random']
-handler.command = /^(meme)$/i
-export default handler
